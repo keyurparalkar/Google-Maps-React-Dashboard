aa92de6a77e408c93362f93bab1ee968332c0230
keyurparalkar <keur.plkar@gmail.com>
2020-09-20 12:07:22 +0530

    Added feature to load map, markers and polylines

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import React, {Component} from 'react';
+import {Container} from '@material-ui/core';
 import GoogleMapReact from 'google-map-react';
 
 import './App.css';
 
-
-class LoadGoogleMap extends Component{
+class SideDrawer extends Component{
   constructor(props){
     super(props)
   }
 
   render(){
     return(
+      <h1>SideBar component</h1>
+    )
+  }
+}
+
+class Marker extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div style={{backgroundColor:'red',width:20, height:20}}>
+        {this.props.text}
+      </div>
+    )
+  }
+}
+class LoadGoogleMap extends Component{
+  constructor(props){
+    super(props)
+
+    this.handleMapObjs = this.handleMapObjs.bind(this)
+  }
+
+  handleMapObjs(args){
+    let fligthPathPoints = [
+      {lat:15.527665, lng:74.907585},
+      {lat:15.503681666666667, lng:74.95155666666666},
+      {lat:13.6113, lng:76.95476833333333}
+    ]
+
+    const fligthPath = new args.maps.Polyline({
+      path:fligthPathPoints,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    })
+
+    fligthPath.setMap(args.map)
+  }
+
+  render(){
+    return(
       <div style={{height:'100vh', width:'100%'}}>
         <h1> Google Maps</h1>
-        <GoogleMapReact 
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-        />
+          <GoogleMapReact 
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals 
+          onGoogleApiLoaded={this.handleMapObjs}
+          >
+
+            <Marker lat={15.527665} lng={74.907585} text="A"/>
+            <Marker lat={13.6113} lng={76.95476833333333} text="B"/>
+          </GoogleMapReact>
       </div>
     )
   }
@@ -25,19 +76,17 @@ class LoadGoogleMap extends Component{
 
 LoadGoogleMap.defaultProps = {
   center:{
-    lat:59,
-    lng:30
+    lat:15.527665,
+    lng:74.907585
   },
-  zoom: 10
+  zoom: 7
  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <LoadGoogleMap />
-       </header>
-    </div>
+    <Container className="App">
+        <LoadGoogleMap/>
+    </Container>
   );
 }
 

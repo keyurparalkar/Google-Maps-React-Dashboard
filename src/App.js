728eac560b159f1474a4b6214ebbd0d3907ce6f1
keyurparalkar <keur.plkar@gmail.com>
2020-09-20 09:44:54 +0530

    Basic map loaded to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
+
 import './App.css';
+
+
+class LoadGoogleMap extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div style={{height:'100vh', width:'100%'}}>
+        <h1> Google Maps</h1>
+        <GoogleMapReact 
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+        />
+      </div>
+    )
+  }
+}
+
+LoadGoogleMap.defaultProps = {
+  center:{
+    lat:59,
+    lng:30
+  },
+  zoom: 10
+ }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <LoadGoogleMap />
+       </header>
     </div>
   );
 }

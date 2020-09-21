8c84df3225f61c9235b6e91c03c143f100d43445
keyurparalkar <keur.plkar@gmail.com>
2020-09-21 20:12:55 +0530

    Changed the Marker

--- src/App.js
+++ src/App.js
@@ -159,10 +159,10 @@ class LoadGoogleMap extends Component{
 
     const arrow = {
       anchor: new args.maps.Point(10,10),
-      strokeColor:'black',
-      strokeWeigth:10,
-      scale:2,
-      fillColor:'black',
+      fillOpacity:1,
+      strokeWeigth:1,
+      scale:1.5,
+      fillColor:'#FFFFFF',
       path:'M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z'
     }
 
@@ -173,7 +173,7 @@ class LoadGoogleMap extends Component{
       geodesic: false,
       strokeColor: "#f0a500",
       strokeOpacity: 2.0,
-      strokeWeight: 3
+      strokeWeight: 4
     })
 
     this.setState({

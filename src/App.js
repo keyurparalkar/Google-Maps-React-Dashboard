2b68bb1b7b3d90795b9952a3de5030d79af158e1
keyurparalkar <keur.plkar@gmail.com>
2020-09-21 18:28:19 +0530

    Added Symbols/Icons for Markers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
-import {Container, Button, Slider} from '@material-ui/core';
+import {Container, Button, Slider,Drawer} from '@material-ui/core';
 import GoogleMapReact from 'google-map-react';
 
 import jsonData from './location-158393297483727762_5th_9th_june.json';
 import './App.css';
 
-const marks = [200,400,600,800,1000]
+
+const marks = [
+  {
+    value: 1,
+    label: '1x',
+  },
+  {
+    value: 2,
+    label: '2x',
+  },
+  {
+    value: 3,
+    label: '3x',
+  },
+  {
+    value: 4,
+    label: '4x',
+  },
+  {
+    value: 5,
+    label: '5x',
+  }
+];
 
 class SideDrawer extends Component{
   constructor(props){
@@ -31,17 +53,18 @@ class LoadGoogleMap extends Component{
       polyline: null,
       map:null,
       maps:null,
-      sliderVal: 1000,
+      sliderVal: 1,
       arrayPos:null,
       timerId:null,
-      trackStatus:'playing'
+      trackStatus:'playing',
+      disabled:false
      }
 
     this.handleMapObjs = this.handleMapObjs.bind(this);
     this.getJSONData = this.getJSONData.bind(this);
     this.startTracking = this.startTracking.bind(this);
     this.pauseTracking = this.pauseTracking.bind(this);
-    // this.stopTracking = this.stopTracking.bind(this);
+    this.stopTracking = this.stopTracking.bind(this);
     this.changeSliderValue = this.changeSliderValue.bind(this);
 
   }
@@ -57,28 +80,26 @@ class LoadGoogleMap extends Component{
 
   pauseTracking(){
     this.setState({
-      trackStatus:'paused'
+      trackStatus:'paused',
+      disabled: false
     })
     clearInterval(this.state.timerId)
   }
 
+  stopTracking(){
+    this.state.polyline.setMap(null)
+    this.state.marker.setPosition(null)
+    this.state.polyline.setPath([])
+    clearInterval(this.state.timerId)
+    this.setState({
+      arrayPos:0,
+      timerId:null,
+      disabled:false
+    })
+  }
+
   startTracking(){
     var timerid = null
-
-    // for(let i=0;i<this.state.fligthPathPoints.length; i++){
-    //   // clearInterval(timerid)
-    //   var delay = this.state.sliderVal
-    //   this.setState({
-    //     timer:i
-    //   })
-    //   timerid = setTimeout(function(coords){
-    //     let latlng = new this.state.maps.LatLng(coords.lat, coords.lng)
-    //     this.state.polyline.getPath().push(latlng)
-    //     this.state.marker.setPosition(latlng) 
-    //     this.state.map.panTo(latlng)
-    //     this.state.polyline.setMap(this.state.map)
-    //   }.bind(this),2*i, this.state.fligthPathPoints[i])
-    // }
 
     let i =0;
     let fligthLen = this.state.fligthPathPoints.length
@@ -98,11 +119,13 @@ class LoadGoogleMap extends Component{
         if(i==fligthLen - 1){
           clearInterval(timerid)
         }
-        i++;
+
+        i = i + this.state.sliderVal;
 
         this.setState({
           arrayPos: i,
-          timerId: timerid
+          timerId: timerid,
+          disabled:true
         })
         
     }.bind(this), 10)
@@ -132,10 +155,12 @@ class LoadGoogleMap extends Component{
     let fligthPathPoints = this.getJSONData()
 
     const arrow = {
-      strokeColor:'white',
-      strokeWeigth:1,
-      scale:3,
-      path:args.maps.SymbolPath.CIRCLE
+      anchor: new args.maps.Point(10,10),
+      strokeColor:'black',
+      strokeWeigth:10,
+      scale:2,
+      fillColor:'black',
+      path:'M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z'
     }
 
     const marker = new args.maps.Marker({map:args.map, icon:arrow})
@@ -143,20 +168,10 @@ class LoadGoogleMap extends Component{
     const fligthPath = new args.maps.Polyline({
       path:[],
       geodesic: false,
-      strokeColor: "#FF0000",
+      strokeColor: "#f0a500",
       strokeOpacity: 2.0,
-      strokeWeight: 2
+      strokeWeight: 3
     })
-
-    // for(let i=0;i<fligthPathPoints.length; i++){
-    //   setTimeout(function(coords){
-    //     let latlng = new args.maps.LatLng(coords.lat, coords.lng)
-    //     fligthPath.getPath().push(latlng)
-    //     marker.setPosition(latlng)
-    //     args.map.panTo(latlng)
-    //     fligthPath.setMap(args.map)
-    //   },i, fligthPathPoints[i])
-    // }
 
     this.setState({
       fligthPathPoints:fligthPathPoints,
@@ -171,20 +186,22 @@ class LoadGoogleMap extends Component{
   
 
   render(){
+    console.log(this.state.sliderVal)
     return(
       <div style={{height:'100vh', width:'100%'}}>
         <h1> Google Maps = {this.state.arrayPos}</h1>
-        <Button variant="contained" onClick={this.startTracking}>START</Button>
+        <Button variant="contained" onClick={this.startTracking} disabled={this.state.disabled}>START/RESUME</Button>
         <Button variant="contained" onClick={this.pauseTracking}>PAUSE</Button>
+        <Button variant="contained" onClick={this.stopTracking}>STOP</Button>
         <Slider 
-        defaultValue={600}
-        step={200}
-        aria-labelledby="discrete-slider"
-        getAriaValueText={(val)=>'${val}x'}
+        defaultValue={1}
+        step={1}
+        aria-labelledby="discrete-slider-custom"
+        getAriaValueText={(val)=>`${val}x`}
         valueLabelDisplay="auto"
-        min={200}
-        max={1000}
-        marks
+        min={1}
+        max={5}
+        marks={marks}
         onChange={this.changeSliderValue}
         />
           <GoogleMapReact 
@@ -205,7 +222,7 @@ LoadGoogleMap.defaultProps = {
     lat:15.527665,
     lng:74.907585
   },
-  zoom: 7
+  zoom: 10
  }
 
 function App() {
